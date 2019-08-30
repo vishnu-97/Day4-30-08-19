f1f72f2b1f875f765b192e47461dde2a1895e1c2
dell <dell@DESKTOP-HQV41S6>
2019-08-30 18:28:34 +0530

    Employee Management

---
+++ src/com/worldpay/EmployeeMangement.java
@@ -0,0 +1,97 @@
+package com.worldpay;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.ListIterator;
+
+public class EmployeeMangement {
+	private static ArrayList<Employee> ea=new ArrayList<>();
+	private static HashMap<String, ArrayList<Employee>> h=new HashMap<>();
+	
+	private static int createEno() {
+		if(!ea.isEmpty())
+			return ea.get(ea.size()-1).getEno()+1;
+		else
+			return 1;
+	}
+	
+	
+	static void addEmployee(Employee e) {
+		e.setEno(createEno());
+		ea.add(e);
+		addToDepartment(e);
+		System.out.println("Your Employee Id ="+e.getEno());
+	}
+	
+	
+	static void viewAllEmployees() {
+		ListIterator<Employee> it=ea.listIterator();
+		System.out.println("Employeeno   \tName \t\tSalary \tDesignation \tDepartment");
+		while(it.hasNext()) {
+			Employee e=it.next();
+			System.out.println(e.getEno() + " \t\t" + e.getEname()+ " \t\t" + e.getSalary() + " \t" + e.getDesignation()+ " \t\t"+e.getDepartment());
+		}
+	}
+	
+	
+	static void removeEmployee(int id) {
+		Employee e=searchEmployee(id);
+		if(e!=null) {
+			System.out.println(e.getEname()+" was removed");
+			ea.remove(e);
+		}
+		else {
+			System.out.println("Invalid EmployeeNo");
+		}
+	}
+
+	static void clearData() {
+		ea.clear();
+	}
+	static void changeSalary(int id,int sal) {
+		Employee e=searchEmployee(id);
+		
+		if(e!=null) {
+			e.setSalary(sal);
+			System.out.println(e.getEname()+" salary was changed");
+				
+		}
+		else {
+			System.out.println("Invalid EmployeeNo");
+		}
+		
+	}
+	
+	static Employee searchEmployee(int id) {
+		ListIterator<Employee> it=ea.listIterator();
+		while(it.hasNext()) {
+			Employee e=it.next();
+			if(e.getEno()==id) {
+				return e;
+			}
+		}
+		return null;
+	}
+	
+//	add employee to department map
+	static void addToDepartment(Employee e) {
+		if(!h.containsKey(e.getDepartment())) {
+
+			h.put(e.getDepartment(), new ArrayList<>());
+			
+		}
+		h.get(e.getDepartment()) .add(e);
+	}
+//	department search
+	static void viewDepartmentWise(String a) {
+		System.out.println("Employeeno   \tName \t\tSalary \tDesignation \tDepartment");
+		
+			ListIterator<Employee> it=h.get(a).listIterator();
+			while(it.hasNext()) {
+				Employee e=it.next();
+				System.out.println(e.getEno() + " \t\t" + e.getEname()+ " \t\t" + e.getSalary() + " \t" + e.getDesignation()+ " \t\t"+e.getDepartment());
+				
+			}
+		
+	}
+}
---
+++ src/com/worldpay/Menu.java
@@ -0,0 +1,81 @@
+package com.worldpay;
+
+import java.util.Scanner;
+
+public class Menu {
+
+	public static void main(String[] args) {
+		Scanner in =new Scanner(System.in );
+		do {
+			System.out.println("\n\nPlease Enter"
+					+ "\n1.Add Employee"
+					+ "\n2.View All Employees"
+					+ "\n3.Remove Employee"
+					+ "\n4.Clear Data"
+					+ "\n5.Change Salary"
+					+ "\n6.Search Employee"
+					+ "\n7.View Depart"
+					+ "\n8.Exit");
+			int o=in.nextInt();
+			switch(o) {
+			case 1:{
+				in.nextLine();
+				System.out.print("Name=");
+				String n=in.nextLine();
+				System.out.print("Salary="); 
+				int s=in.nextInt();
+				in.nextLine();
+				
+				System.out.print("Designation="); 
+				String des=in.nextLine();
+				System.out.print("Department="); 
+				String dep=in.nextLine();
+				EmployeeMangement.addEmployee(new Employee(n, s, des, dep));
+				break;
+			}
+			case 2:{
+				EmployeeMangement.viewAllEmployees();
+				break;
+			}
+			case 3:{
+				System.out.print("EmployeeNo="); 
+				int id=in.nextInt();
+				EmployeeMangement.removeEmployee(id);
+				break;
+			}
+			case 4:{
+				EmployeeMangement.clearData();
+				break;
+			}	
+			case 5:{
+				System.out.print("EmployeeNo="); 
+				int id=in.nextInt();
+				System.out.print("Salary="); 
+				int sal=in.nextInt();
+				EmployeeMangement.changeSalary(id, sal);;
+				break;
+			}
+			case 6:{
+				System.out.print("EmployeeNo="); 
+				int id=in.nextInt();
+				Employee e=EmployeeMangement.searchEmployee(id);
+				if(e!=null)
+				System.out.println(e);
+				break;
+			}
+			case 7:{
+				System.out.print("Department="); 
+				in.nextLine();
+				
+				String dep=in.nextLine();
+				EmployeeMangement.viewDepartmentWise(dep);
+				break;
+			}
+			case 8:{
+				System.exit(0);
+			}
+			}	
+		}while(true);
+	}
+
+}
